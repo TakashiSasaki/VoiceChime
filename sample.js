10022c363c25928f8362fc99ff29f304b1e027b3
Takashi SASAKI <takashi316@gmail.com>
2018-02-24 05:45:58 +0900

    reformat code

--- sample.js
+++ sample.js
@@ -1,12 +1,16 @@
 // https://qiita.com/ktetsuo/items/8c9cd5714e231aa6ae09
 
 const googlehome = require('google-home-notifier')
-const language = 'ja';
 
-googlehome.device('Google-Home', language); 
+var messages = [];
+messages.push('こんにちは。私はグーグルホームです。');
+messages.push('みなさん、お元気ですか？');
+var message = messages.join("");
+
+googlehome.device('Google-Home', 'ja'); 
 googlehome.ip("192.168.0.5")
 
-googlehome.notify('こんにちは。私はグーグルホームです。あゆなさん、お元気ですか？', function(res) {
+googlehome.notify(message, function(res) {
 	  console.log(res);
 });
 
